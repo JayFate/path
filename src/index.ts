a1eee0b96df1d0edf0561b3ebeb09d7b7ee4c418
JayFate <48240828+JayFate@users.noreply.github.com>
2022-10-04 14:37:35 +0800

    feat: update types
    Signed-off-by: JayFate <48240828+JayFate@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,77 @@
 import path, { PlatformPath } from "path";
 // const path = require("path")
 
-const path2 = Object.create(path)
-
-const replaceStr = (str: string) => str.replace(/\\/gm, '/')
+const replaceStr = (str: string): string => str.replace(/\\/gm, '/')
 const isFunction = (o: any) => typeof o === "function"
 const isString = (o: any) => typeof o === "string"
 
-// console.log(Object.keys(path))
+/**
+wrap 方法接收函数 fn，并返回一个包装后的函数 fn1，fn1 与 fn 的接口完全相同
+ */
 
-for (const key in path) {
-    // console.log(`key`, key, `typeof path[key]`, typeof path[key])
-    // console.log(path[key])
-
-    if (isFunction(path[key as keyof typeof path])) {
-        path2[key] = function (...argus: any[]) {
-            argus = argus.map(str => replaceStr(str))
-            let res = (path[key as keyof typeof path] as Function)(...argus)
-            res = replaceStr(res)
-            return res
-        }
-
-    } else if (isString(path[key as keyof typeof path])) {
-        path2[key] = replaceStr(path[key as keyof typeof path] as string)
-    } else {
-        path2[key] = path[key as keyof typeof path]
+const wrap = (fn: any) => {
+    return function (...argus: any[]) {
+        argus = argus.map(str => replaceStr(str))
+        let res = fn(...argus)
+        res = replaceStr(res)
+        return res
     }
 }
+// export const _makeLong: typeof path._makeLong = wrap(path._makeLong)
+export const resolve: typeof path.resolve = wrap(path.resolve)
+export const normalize: typeof path.normalize = wrap(path.normalize)
+export const isAbsolute: typeof path.isAbsolute = wrap(path.isAbsolute)
+export const join: typeof path.resolve = wrap(path.join)
+export const relative: typeof path.relative = wrap(path.relative)
+export const toNamespacedPath: typeof path.toNamespacedPath = wrap(path.toNamespacedPath)
+export const dirname: typeof path.dirname = wrap(path.dirname)
+export const basename: typeof path.basename = wrap(path.basename)
+export const extname: typeof path.extname = wrap(path.extname)
+export const format: typeof path.format = wrap(path.format)
+export const parse: typeof path.parse = wrap(path.parse)
+export const sep = "/"
+export const delimiter: typeof path.delimiter = path.delimiter
+export const win32: typeof path.win32 = path.win32
+export const posix: typeof path.posix = path.posix
+
+
+// console.log(Object.keys(path))
+const path2 = {
+    resolve,
+    normalize,
+    isAbsolute,
+    join,
+    relative,
+    toNamespacedPath,
+    dirname,
+    basename,
+    extname,
+    format,
+    parse,
+    sep,
+    delimiter,
+    win32,
+    posix,
+}
+
+// for (const key in path) {
+//     // console.log(`key`, key, `typeof path[key]`, typeof path[key])
+//     // console.log(path[key])
+
+//     if (isFunction(path[key as keyof typeof path])) {
+//         path2[key] = function (...argus: any[]) {
+//             argus = argus.map(str => replaceStr(str))
+//             let res = (path[key as keyof typeof path] as Function)(...argus)
+//             res = replaceStr(res)
+//             return res
+//         }
+
+//     } else if (isString(path[key as keyof typeof path])) {
+//         path2[key] = replaceStr(path[key as keyof typeof path] as string)
+//     } else {
+//         path2[key] = path[key as keyof typeof path]
+//     }
+// }
 
 module.exports = path2
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,35 @@
-declare const path2: any;
+/// <reference types="node" />
+import path from "path";
+export declare const resolve: typeof path.resolve;
+export declare const normalize: typeof path.normalize;
+export declare const isAbsolute: typeof path.isAbsolute;
+export declare const join: typeof path.resolve;
+export declare const relative: typeof path.relative;
+export declare const toNamespacedPath: typeof path.toNamespacedPath;
+export declare const dirname: typeof path.dirname;
+export declare const basename: typeof path.basename;
+export declare const extname: typeof path.extname;
+export declare const format: typeof path.format;
+export declare const parse: typeof path.parse;
+export declare const sep = "/";
+export declare const delimiter: typeof path.delimiter;
+export declare const win32: typeof path.win32;
+export declare const posix: typeof path.posix;
+declare const path2: {
+    resolve: (...paths: string[]) => string;
+    normalize: (path: string) => string;
+    isAbsolute: (path: string) => boolean;
+    join: (...paths: string[]) => string;
+    relative: (from: string, to: string) => string;
+    toNamespacedPath: (path: string) => string;
+    dirname: (path: string) => string;
+    basename: (path: string, ext?: string | undefined) => string;
+    extname: (path: string) => string;
+    format: (pathObject: path.FormatInputPathObject) => string;
+    parse: (path: string) => path.ParsedPath;
+    sep: string;
+    delimiter: ";" | ":";
+    win32: path.PlatformPath;
+    posix: path.PlatformPath;
+};
 export default path2;
