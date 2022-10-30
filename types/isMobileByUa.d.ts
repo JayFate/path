74aab11259652eb659494d604b8bc63226cc7317
JayFate <37610029@qq.com>
2022-10-30 04:04:00 +0000

    fix: tsconfig include src
    Signed-off-by: JayFate <37610029@qq.com>

--- types/isMobileByUa.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 通过检测设备 ua
- * 判断是否是移动端设备
- * @returns {boolean}
- */
-export declare function isMobileByUa(userAgent?: string): boolean;
--- types/padZero.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 向 < 10 的整形数值进行补 0
- * @param {number} n
- * @returns {string}
- */
-export declare function padZero(n: number): string | number;
